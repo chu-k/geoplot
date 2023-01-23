fb3ae3d8432959a64fd1d5401af427b9a587e551
Kevin C <kvnchv@gmail.com>
2023-01-22 19:02:23 -0600

    feat: basic triangle methods, visualization notebook

--- geoplot/base/shape.py
+++ geoplot/base/shape.py
@@ -1,6 +1,6 @@
 from abc import ABC
-from typing import Union
 
+import numpy as np
 from numpy.typing import ArrayLike
 
 class Shape2d(ABC):
@@ -10,3 +10,22 @@ class Shape2d(ABC):
 
     def get_center(self) -> ArrayLike:
         return self.center
+
+    def create_npoints(self, inscribe_r:float):
+        """https://math.stackexchange.com/questions/4114500"""
+        points = []
+        theta_k = 0.0
+        n = self.num_points
+        for k in range(n):
+            theta_k = 2.0 * k * np.pi /n
+            pt = self.center + inscribe_r / (2.0 * np.sin(np.pi/n)) * np.array((np.cos(theta_k), np.sin(theta_k)))
+            points.append(pt)
+        return np.array(points)
+
+    def rotate2d(self, angle:float):
+        """Rotate about centroid by angle."""
+        shifted = self.points - self.center
+        rot = np.array([[np.cos(angle), -np.sin(angle)],
+                        [np.sin(angle), np.cos(angle)]])
+        shifted = shifted @ rot.T + self.center
+        self.points = shifted
--- tests/test_geoplot.py
+++ tests/test_geoplot.py
@@ -6,5 +6,7 @@ def test_version():
     assert __version__ == "0.1.0"
 
 def test_triangle():
-    tri = geoplot.triangle.Triangle(np.random.random(2), 5)
+    tri = geoplot.triangle.Triangle(np.array((0,0)), 1)
+    print(tri.points)
+    tri.rotate2d(np.radians(90))
     print(tri.points)
